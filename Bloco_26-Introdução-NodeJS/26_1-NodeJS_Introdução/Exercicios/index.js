b1bcfc38148a0ed262868bd8a92c159bac20a49d
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-26 10:38:42 -0300

    26_1-2 - adicionando conteudo - [DONE]

--- Bloco_26-Introdução-NodeJS/26_1-NodeJS_Introdução/Exercicios/index.js
+++ Bloco_26-Introdução-NodeJS/26_1-NodeJS_Introdução/Exercicios/index.js
@@ -1,1 +1,14 @@
 const readline = require('readline-sync');
+
+const choices = ['imc', 'velocidade', 'loteria'];
+
+const useChoice = readline.keyInSelect(choices, 'Escolha o arquivo:');
+
+switch (useChoice) {
+  case choices.indexOf('imc'):
+    const imc = require('./imc');
+  case choices.indexOf('velocidade'):
+    const velocidade = require('./velocidade');
+  case choices.indexOf('loteria'):
+    const loteria = require('./loteria');
+}
