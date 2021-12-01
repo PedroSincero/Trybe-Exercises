ea705b39a0e7e5893c191da3fe3d21792cf71903
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-01 01:05:36 -0300

    build - adicionando estrutura inicial exercicios

---
+++ Bloco_32-Introducao_a_python/32_2-Entrada-e-saida-de-Dados/Exercicios/exercicio1.py
@@ -0,0 +1,9 @@
+# Exercício 1: Faça um programa que receba um nome e imprima o mesmo na vertical em escada invertida. Exemplo:
+# Entrada:
+# PEDRO
+# Saída:
+# PEDRO
+# PEDR
+# PED
+# PE
+# P
---
+++ Bloco_32-Introducao_a_python/32_2-Entrada-e-saida-de-Dados/Exercicios/exercicio3.py
@@ -0,0 +1,1 @@
+# Exercício 3: Modifique o exercício anterior para que as palavras sejam lidas de um arquivo. Considere que o arquivo terá cada palavra em uma linha.
